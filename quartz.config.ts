44df8b566219b90aaa9586e787c62138f50ad396
pmcf-percona <42041153+freenandes@users.noreply.github.com>
2023-10-11 17:59:06 +0100

    blank

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,11 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "FREENANDES",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    baseUrl: "quartz.jzhao.xyz",
+    analytics: null,
+    baseUrl: "pmcf.xyz/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
